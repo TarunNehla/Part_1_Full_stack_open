0808c8c4c1d91ae81b62ef20d45bd6b7b103878f
Tarun Dhillon <tarundhillon14330@gmail.com>
2023-11-01 21:37:39 +0530

    1.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ const Header = () =>{
   return <h1>{course}</h1>
 }
 
-const Body  = (props) =>{
+const Part = (props) =>{
   return <p>{props.part} {props.exercises}</p>
+}
+
+
+const Body  = (props) =>{
+  return (
+    <>
+      <Part part = {props.part[0]} exercises = {props.exercises[0]}/>
+      <Part part = {props.part[1]} exercises = {props.exercises[1]}/>
+      <Part part = {props.part[2]} exercises = {props.exercises[2]}/>
+    </>
+  )
 }
 
 const Total = (props) =>{
@@ -20,13 +31,12 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const parts = [part1, part2, part3]
   const exercises = [exercises1, exercises2, exercises3]
   return (
     <div>
       <Header/>
-      <Body part = {part1} exercises = {exercises1} />
-      <Body part = {part2} exercises = {exercises2} />
-      <Body part = {part3} exercises = {exercises3} />
+      <Body part = {parts} exercises = {exercises}/>
       <Total exercises = {exercises} />
     </div>
   )
