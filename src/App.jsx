a4367dbaea93e9ef142fd8bc63d6e0737677a4ce
Tarun Dhillon <tarundhillon14330@gmail.com>
2023-11-04 16:10:10 +0530

    1.11

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,34 @@ const Statistics = ({good, neutral, bad, total}) => {
     )
   }
   return (
-    <>
-      <StatisticsLine text = 'good' value = {good}/>
-      <StatisticsLine text = 'neutral' value = {neutral}/>
-      <StatisticsLine text = 'bad' value = {bad}/>
-      <StatisticsLine text = 'all' value = {total}/>
-      <StatisticsLine text = 'avergae' value = {(good-bad)/total}/>
-      <StatisticsLine text = 'positive' value = {good/total*100}/>
-    </>
+    <table>
+      <tbody>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>all</td>
+        <td>{total}</td>
+      </tr>
+      <tr>
+        <td>Average</td>
+        <td>{(good-bad)/total}</td>
+      </tr>
+      <tr>
+        <td>positive</td>
+        <td>{good/total*100}</td>
+      </tr>
+      </tbody>
+    </table>
   )
 }
 
